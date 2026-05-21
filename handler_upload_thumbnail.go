83236df8c5b813472c50bee461d04f6feabad7cb
kftlfd <kftlfd@ex.ua>
2026-05-21 13:49:30 +0300

    save thumbnail to fs

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -67,10 +69,33 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	// }
 	// thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
 
-	thumbnailStr := base64.StdEncoding.EncodeToString(imageData)
-	thumbnailURL := fmt.Sprintf("data:%s;base64,%s", mediaType, thumbnailStr)
-	video.ThumbnailURL = &thumbnailURL
+	// thumbnailStr := base64.StdEncoding.EncodeToString(imageData)
+	// thumbnailURL := fmt.Sprintf("data:%s;base64,%s", mediaType, thumbnailStr)
 
+	_, fileExt, ok := strings.Cut(mediaType, "/")
+	if !ok || len(fileExt) < 1 {
+		respondWithError(w, http.StatusBadRequest, "Unknown file extention", nil)
+		return
+	}
+
+	filePath := filepath.Join(cfg.assetsRoot, videoID.String()+"."+fileExt)
+
+	assetFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating asset file", err)
+		return
+	}
+	defer assetFile.Close()
+
+	_, err = assetFile.Write(imageData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error writing asset file", err)
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID, fileExt)
+
+	video.ThumbnailURL = &thumbnailURL
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error updating video", err)
